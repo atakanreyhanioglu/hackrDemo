d1cf2ce27b8f9353b01e236129e4d540decdddb5
Atakan Reyhanioglu <atakan.reyhanioglu@gmail.com>
2021-05-28 00:04:38 +0300

    restored node

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,37 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Head from 'next/head';
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
 
 const Layout = ({ children }) => {
     const head = () => (
-        <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-            crossOrigin="anonymous"
-        />
+        <React.Fragment>
+            <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                crossorigin="anonymous"
+            />
+            <link rel="stylesheet" href="/static/css/styles.css" />
+        </React.Fragment>
     );
 
     const nav = () => (
-        <ul className="nav nav-tabs bg-warning">
-            <li className="nav-item">
+        <div className="container">
+        <ul className="nav nav-tabs bg-light">
+             <li id="navlist" className="nav-item">
                 <Link href="/">
-                    <a className="nav-link text-dark">Home</a>
+                <img src="https://i.ibb.co/mzjCZKW/from-the-millers-2.jpg" height="80"/>
+
+                </Link>
+            </li >
+            <li id="navlist" className="nav-item">
+                <Link href="/">
+                    <a id="navtext"className="nav-link text-dark"><b>Home</b></a>
+                </Link>
+            </li >
+            <li id="navlist" className="nav-item">
+                <Link href="/">
+                    <a id="navtext"className="nav-link text-dark">Documents</a>
                 </Link>
             </li>
-            <li className="nav-item">
+            <li id="navlist" className="nav-item">
+                <Link href="/">
+                    <a id="navtext"className="nav-link text-dark">About Us</a>
+                </Link>
+            </li>
+            <li id="navlist" className="nav-item">
                 <Link href="/login">
-                    <a className="nav-link text-dark">Login</a>
+                    <a id="navtextlogin"className="nav-link text-dark"><b>Login</b></a>
                 </Link>
             </li>
-            <li className="nav-item">
+            <li  id="navlist" className="nav-item">
                 <Link href="/register">
-                    <a className="nav-link text-dark">Register</a>
+                    <a id="navtext"className="btn btn-warning">Register</a>
                 </Link>
             </li>
         </ul>
+        </div>
     );
 
     return (
